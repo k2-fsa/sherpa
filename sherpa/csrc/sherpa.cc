42a9b38fc06cbe204c6dab15ba6f197134bf6fbd
Fangjun Kuang <csukuangfj@gmail.com>
2022-08-14 16:35:28 +0800

    WIP: Add greedy search for non-streaming ASR

--- sherpa/csrc/sherpa.cc
+++ sherpa/csrc/sherpa.cc
@@ -18,9 +18,58 @@
 #include <iostream>
 
 #include "kaldifeat/csrc/feature-fbank.h"
+#include "sherpa/csrc/file_utils.h"
+#include "sherpa/csrc/log.h"
+#include "sherpa/csrc/parse_options.h"
 
-int main() {
-  kaldifeat::FbankOptions opts;
-  std::cout << opts.ToString() << "\n";
+struct Options {
+  /// Path to torchscript model
+  std::string nn_model;
+
+  /// Path to BPE model file. Used only for BPE based models
+  std::string bpe_model;
+
+  void Register(sherpa::ParseOptions *po) {
+    po->Register("nn-model", &nn_model, "Path to the torchscript model");
+    po->Register("bpe-model", &bpe_model,
+                 "Path to the BPE model. Used only for BPE based models");
+  }
+
+  void Validate() const {
+    if (nn_model.empty()) {
+      SHERPA_LOG(FATAL) << "Please provide --nn-model";
+    }
+
+    if (!sherpa::FileExists(nn_model)) {
+      SHERPA_LOG(FATAL) << "\n--nn-model=" << nn_model << "\n"
+                        << nn_model << " does not exist!";
+    }
+  }
+
+  std::string ToString() const {
+    std::ostringstream os;
+    os << "--nn-model=" << nn_model << "\n";
+    if (!bpe_model.empty()) {
+      os << "--bpe-model=" << bpe_model << "\n";
+    }
+    return os.str();
+  }
+};
+
+int main(int argc, char *argv[]) {
+  const char *usage = R"(Automatic speech recognition with sherpa.
+Usage:
+  ./bin/sherpa --help
+  )";
+  sherpa::ParseOptions po(usage);
+
+  Options opts;
+  opts.Register(&po);
+
+  po.Read(argc, argv);
+  opts.Validate();
+
+  SHERPA_LOG(INFO) << "\n" << opts.ToString();
+
   return 0;
 }
