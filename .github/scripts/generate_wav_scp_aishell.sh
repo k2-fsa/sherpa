01a68847b0ec179e5a0cba34a2e25990dae25023
Fangjun Kuang <csukuangfj@gmail.com>
2022-08-16 23:01:43 +0800

    fix a typo

--- .github/scripts/generate_wav_scp_aishell.sh
+++ .github/scripts/generate_wav_scp_aishell.sh
@@ -2,5 +2,5 @@
 cat > wav_aishell.scp <<EOF
 wav1 icefall-aishell-pruned-transducer-stateless3-2022-06-20/test_wavs/BAC009S0764W0121.wav
 wav2 icefall-aishell-pruned-transducer-stateless3-2022-06-20/test_wavs/BAC009S0764W0122.wav
-wav 3icefall-aishell-pruned-transducer-stateless3-2022-06-20/test_wavs/BAC009S0764W0123.wav
+wav3 icefall-aishell-pruned-transducer-stateless3-2022-06-20/test_wavs/BAC009S0764W0123.wav
 EOF
