1f05bb74cd1a63f7a261ae9be950e297b6c189b0
Fangjun Kuang <csukuangfj@gmail.com>
2022-11-06 15:35:48 +0800

    small fixes

--- sherpa/cpp_api/bin/offline-recognizer.cc
+++ sherpa/cpp_api/bin/offline-recognizer.cc
@@ -1,23 +1,10 @@
-/**
- * Copyright      2022  Xiaomi Corporation (authors: Fangjun Kuang)
- *
- * See LICENSE for clarification regarding multiple authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// sherpa/cpp_api/bin/offline-recognizer.h
+//
+// Copyright (c)  2022  Xiaomi Corporation
+#include "sherpa/cpp_api/offline-recognizer.h"
+
 #include <iostream>
 
-#include "sherpa/cpp_api/offline-recognizer.h"
 #include "sherpa/cpp_api/parse-options.h"
 #include "torch/all.h"
 
@@ -26,9 +13,9 @@ Decode wave file(s) using offline recognizer from sherpa.
 
 Usage:
 
-./bin/test-decode-files --help
+./bin/sherpa-offline-recongizer --help
 
-./bin/test-decode-files \
+./bin/sherpa-offline-recognizer \
   --use-gpu=false \
   --nn-model=/path/to/cpu.jit \
   --tokens=/path/to/tokens.txt \
@@ -94,7 +81,7 @@ int main(int argc, char *argv[]) {
     os << "filename: " << po.GetArg(1) << "\n";
     os << "text: " << r.text << "\n";
     os << "token IDs: " << r.tokens << "\n";
-    os << "timestamps (after subsampling): " << r.timestamps << "\n";
+    os << "timestamps (after subsampling): " << r.timestamps << "\n\n";
   }
   std::cout << os.str() << "\n";
   return 0;
