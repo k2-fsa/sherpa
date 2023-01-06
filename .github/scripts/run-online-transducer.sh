9fba16922975db5886f00e8622bcc3acdd7b92c3
yaozengwei <yaozengwei@outlook.com>
2023-01-06 11:52:32 +0800

    add CI scripts

--- .github/scripts/run-online-transducer.sh
+++ .github/scripts/run-online-transducer.sh
@@ -9,6 +9,49 @@ log() {
 }
 
 log "=========================================================================="
+
+repo_url=https://huggingface.co/Zengwei/icefall-asr-librispeech-pruned-transducer-stateless7-streaming-2022-12-29
+log "Start testing ${repo_url}"
+repo=$(basename $repo_url)
+log "Download pretrained model and test-data from $repo_url"
+
+GIT_LFS_SKIP_SMUDGE=1 git clone $repo_url
+pushd $repo
+git lfs pull --include "exp/encoder_jit_trace.pt"
+git lfs pull --include "exp/decoder_jit_trace.pt"
+git lfs pull --include "exp/joiner_jit_trace.pt"
+git lfs pull --include "data/lang_bpe_500/LG.pt"
+
+for m in greedy_search modified_beam_search fast_beam_search; do
+  time ./build/bin/sherpa-online \
+    --decoding-method=$m \
+    --encoder-model=$repo/exp/encoder_jit_trace.pt \
+    --decoder-model=$repo/exp/decoder_jit_trace.pt \
+    --joiner-model=$repo/exp/joiner_jit_trace.pt \
+    --tokens=$repo/data/lang_bpe_500/tokens.txt \
+    --decode-chunk-size=32 \
+    $repo/test_wavs/1089-134686-0001.wav \
+    $repo/test_wavs/1221-135766-0001.wav \
+    $repo/test_wavs/1221-135766-0002.wav
+done
+
+# For fast_beam_search with LG
+time ./build/bin/sherpa-online \
+  --decoding-method=fast_beam_search \
+  --encoder-model=$repo/exp/encoder_jit_trace.pt \
+  --decoder-model=$repo/exp/decoder_jit_trace.pt \
+  --joiner-model=$repo/exp/joiner_jit_trace.pt \
+  --lg=$repo/data/lang_bpe_500/LG.pt \
+  --tokens=$repo/data/lang_bpe_500/tokens.txt \
+  --decode-chunk-size=32 \
+  $repo/test_wavs/1089-134686-0001.wav \
+  $repo/test_wavs/1221-135766-0001.wav \
+  $repo/test_wavs/1221-135766-0002.wav
+
+rm -rf $repo
+log "End of testing ${repo_url}"
+
+log "=========================================================================="
 repo_url=https://huggingface.co/Zengwei/icefall-asr-librispeech-conv-emformer-transducer-stateless2-2022-07-05
 log "Start testing ${repo_url}"
 repo=$(basename $repo_url)
