329ab3c573252e835844bea38505c6b43e994cf4
Yuekai Zhang <yuekaiz@nvidia.com>
2025-02-16 23:49:28 -0800

    fix

--- triton/f5_tts/build_server.sh
+++ triton/f5_tts/build_server.sh
@@ -11,26 +11,26 @@ F5_TTS_TRT_LLM_ENGINE_PATH=./f5_trt_llm_engine
 python_package_path=/usr/local/lib/python3.12/dist-packages
 
 
-# DIR=$python_package_path/tensorrt_llm/models/f5tts
-# if [ -d "$DIR" ]; then
-#   echo "Directory '$DIR' exists."
-# else
-#   echo "Directory '$DIR' does not exist."
-#   mkdir -p $python_package_path/tensorrt_llm/models/f5tts
-#   wget https://raw.githubusercontent.com/yuekaizhang/TensorRT-LLM/f5/tensorrt_llm/models/__init__.py -O $python_package_path/tensorrt_llm/models/__init__.py
-#   wget https://raw.githubusercontent.com/yuekaizhang/TensorRT-LLM/f5/tensorrt_llm/models/f5tts/model.py -O $python_package_path/tensorrt_llm/models/f5tts/model.py
-#   wget https://raw.githubusercontent.com/yuekaizhang/TensorRT-LLM/f5/tensorrt_llm/models/f5tts/modules.py -O $python_package_path/tensorrt_llm/models/f5tts/modules.py
-# fi
+DIR=$python_package_path/tensorrt_llm/models/f5tts
+if [ -d "$DIR" ]; then
+  echo "Directory '$DIR' exists."
+else
+  echo "Directory '$DIR' does not exist."
+  mkdir -p $python_package_path/tensorrt_llm/models/f5tts
+  wget https://raw.githubusercontent.com/yuekaizhang/TensorRT-LLM/f5/tensorrt_llm/models/__init__.py -O $python_package_path/tensorrt_llm/models/__init__.py
+  wget https://raw.githubusercontent.com/yuekaizhang/TensorRT-LLM/f5/tensorrt_llm/models/f5tts/model.py -O $python_package_path/tensorrt_llm/models/f5tts/model.py
+  wget https://raw.githubusercontent.com/yuekaizhang/TensorRT-LLM/f5/tensorrt_llm/models/f5tts/modules.py -O $python_package_path/tensorrt_llm/models/f5tts/modules.py
+fi
 
-# huggingface-cli download SWivid/F5-TTS --local-dir $F5_TTS_HF_DOWNLOAD_PATH
-# python3 ./scripts/convert_checkpoint.py \
-#         --timm_ckpt "$F5_TTS_HF_DOWNLOAD_PATH/F5TTS_Base/model_1200000.pt" \
-#         --output_dir "$F5_TTS_TRT_LLM_CHECKPOINT_PATH"
-# trtllm-build --checkpoint_dir $F5_TTS_TRT_LLM_CHECKPOINT_PATH --output_dir $F5_TTS_TRT_LLM_ENGINE_PATH
+huggingface-cli download SWivid/F5-TTS --local-dir $F5_TTS_HF_DOWNLOAD_PATH
+python3 ./scripts/convert_checkpoint.py \
+        --timm_ckpt "$F5_TTS_HF_DOWNLOAD_PATH/F5TTS_Base/model_1200000.pt" \
+        --output_dir "$F5_TTS_TRT_LLM_CHECKPOINT_PATH"
+trtllm-build --checkpoint_dir $F5_TTS_TRT_LLM_CHECKPOINT_PATH --output_dir $F5_TTS_TRT_LLM_ENGINE_PATH
 
-# pip install vocos
-# python3 scripts/export_vocoder_to_onnx.py --vocoder $vocoder
-# bash scripts/export_vocos_trt.sh
+pip install vocos
+python3 scripts/export_vocoder_to_onnx.py --vocoder $vocoder
+bash scripts/export_vocos_trt.sh
 
 rm -r $model_repo
 cp -r ./model_repo_f5_tts $model_repo
