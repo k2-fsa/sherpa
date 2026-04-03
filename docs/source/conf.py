58f38cb7dc9a7352fd30664079ea69f4e2ddfbd0
Fangjun Kuang <csukuangfj@gmail.com>
2026-04-03 14:50:51 +0800

    Add doc for Cohere Transcribe  @csukuangfj (#816)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -229,6 +229,7 @@ rst_epilog = f"""
 .. _sherpa_onnx_release_tag: https://github.com/k2-fsa/sherpa-onnx/releases/tag/v{sherpa_onnx_version}
 .. _sherpa_onnx_rknn_static_archive: https://github.com/k2-fsa/sherpa-onnx/releases/download/v{sherpa_onnx_version}/sherpa-onnx-v{sherpa_onnx_version}-rknn-linux-aarch64-static.tar.bz2
 .. _sherpa_onnx_rknn_shared_archive: https://github.com/k2-fsa/sherpa-onnx/releases/download/v{sherpa_onnx_version}/sherpa-onnx-v{sherpa_onnx_version}-rknn-linux-aarch64-shared.tar.bz2
+.. _Cohere Transcribe: https://huggingface.co/CohereLabs/cohere-transcribe-03-2026
 """
 
 
